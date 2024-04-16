1edbe9e5ea805dfecc02215f6d9a9db030e1b8c4
seyitbugraerden <seyitbugraerden@gmail.com>
2024-04-16 14:42:38 +0300

    Before Music List

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import Menu from "./Sidebar/Menu";
 
 function Sidebar() {
   return (
-    <aside className="w-72 py-7">
+    <aside className="w-80 py-7">
       <img src={logo} alt="logo" className="h-6 mb-2 ps-8" />
       <Menu />
     </aside>
--- src/components/Sidebar/Menu.jsx
+++ src/components/Sidebar/Menu.jsx
@@ -1,6 +1,8 @@
 import { IoHomeOutline } from "react-icons/io5";
 import { IoSearch } from "react-icons/io5";
 import { HiOutlineBookmark } from "react-icons/hi";
+import { FaArrowRight } from "react-icons/fa6";
+
 import { FiPlus } from "react-icons/fi";
 import { Link, useLocation } from "react-router-dom";
 import { useEffect } from "react";
@@ -36,20 +38,37 @@ function Menu() {
             Ara
           </Link>
         </li>
+      </ul>
+      <ul className="text-link font-bold mt-6">
         <li className="relative">
           <Link
             to="/collection"
-            className={`flex h-12 gap-x-5 items-center hover:text-white transition rounded delay-100 ps-8 ${
-              location.pathname === "/collection" ? "bg-active text-white" : ""
-            }`}
+            className={`flex h-12 gap-x-5 items-center   rounded  ps-8`}
           >
-            <HiOutlineBookmark style={{ fontSize: "24px" }} />
-            Kitaplığın
+            <span className="hover:text-white transition delay-100 flex gap-x-4 flex-row">
+              <HiOutlineBookmark style={{ fontSize: "24px" }} /> Kitaplığın
+            </span>
+
             <FiPlus
               style={{ fontSize: "20px" }}
-              className="absolute right-0.5"
+              className="absolute right-6  delay-100 transition hover:text-white"
+            />
+            <FaArrowRight
+              style={{ fontSize: "16px" }}
+              className="absolute right-1 delay-100  transition hover:text-white"
             />
           </Link>
+        </li>
+      </ul>
+      <ul className="flex flex-row gap-x-4 pt-1 mx-auto w-100 ps-10">
+        <li>
+          <Link>Çalma Listesi</Link>
+        </li>
+        <li>
+          <Link>Sanatçılar</Link>
+        </li>
+        <li>
+          <Link>Albümler</Link>
         </li>
       </ul>
     </nav>
