39721e72127a2a8a77ab9c1dce659e21167200fe
seyitbugraerden <seyitbugraerden@gmail.com>
2024-04-17 15:21:56 +0300

    sideBar fix

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,8 @@ import Menu from "./Sidebar/Menu";
 function Sidebar() {
   return (
     <aside
-      className="py-7 min-h-80 w-[28%]"
+      className="py-7 min-h-80 w-[350px] flex-shrink"
+      style={{ minWidth: "350px" }}
     >
       <Link to="/">
         <img src={logo} alt="logo" className="h-14 mb-2 ps-8 " />
